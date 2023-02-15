59de8f486f44b52c42c59dd5057dc64be867a8d9
Jan Kaifer <jan@kaifer.cz>
2023-02-15 18:13:34 +0100

    reproduced

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
+"use client";
+
+import { useClient } from "./useClient";
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  useClient();
+  return null;
 }
